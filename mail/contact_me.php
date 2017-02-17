40f46fba120904989232b806e03e8059838a0b52
DESKTOP-S5TTF8T\hugozanini <hugozaninigomes@gmail.com>
2017-02-17 17:01:13 -0200

    Textos
    Textos V 1.0

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'hugozanini@markitt.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato do site de:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: hugozanini@markitt.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
